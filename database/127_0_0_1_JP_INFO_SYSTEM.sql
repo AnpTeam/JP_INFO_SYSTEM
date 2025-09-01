158f83e2f436ad79b83b201c9c6e23257260f6b2
AnuKun <118412797+AnpTeam@users.noreply.github.com>
2025-09-01 20:10:18 +0700

    [config] Add Database Table (SQL)

---
+++ database/127_0_0_1_JP_INFO_SYSTEM.sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 31, 2025 at 04:53 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+CREATE DATABASE IF NOT EXISTS `jp_info_system` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `jp_info_system`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_attraction`
+--
+
+CREATE TABLE `tbl_attraction` (
+  `atrr_id` int(11) NOT NULL,
+  `attr_name` varchar(100) NOT NULL,
+  `attr_thumbnail` varchar(200) NOT NULL,
+  `attr_desc` varchar(300) NOT NULL,
+  `attr_category` varchar(100) NOT NULL,
+  `like_count` int(11) NOT NULL DEFAULT 0,
+  `city_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_city`
+--
+
+CREATE TABLE `tbl_city` (
+  `city_id` int(11) NOT NULL,
+  `city_name` varchar(100) NOT NULL,
+  `region_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_comment`
+--
+
+CREATE TABLE `tbl_comment` (
+  `comment_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `comment_desc` varchar(300) NOT NULL,
+  `attr_id` int(11) NOT NULL,
+  `like_count` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_region`
+--
+
+CREATE TABLE `tbl_region` (
+  `region_id` int(11) NOT NULL,
+  `region_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_password` varchar(200) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `user_phone` int(11) NOT NULL,
+  `user_role` set('user','admin') NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_attraction`
+--
+ALTER TABLE `tbl_attraction`
+  ADD PRIMARY KEY (`atrr_id`),
+  ADD UNIQUE KEY `attr_name` (`attr_name`),
+  ADD UNIQUE KEY `attr_thumbnail` (`attr_thumbnail`),
+  ADD UNIQUE KEY `attr_desc` (`attr_desc`),
+  ADD UNIQUE KEY `attr_category` (`attr_category`);
+
+--
+-- Indexes for table `tbl_city`
+--
+ALTER TABLE `tbl_city`
+  ADD PRIMARY KEY (`city_id`),
+  ADD UNIQUE KEY `city_name` (`city_name`);
+
+--
+-- Indexes for table `tbl_comment`
+--
+ALTER TABLE `tbl_comment`
+  ADD PRIMARY KEY (`comment_id`);
+
+--
+-- Indexes for table `tbl_region`
+--
+ALTER TABLE `tbl_region`
+  ADD PRIMARY KEY (`region_id`),
+  ADD UNIQUE KEY `region_name` (`region_name`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_name` (`user_name`),
+  ADD UNIQUE KEY `user_password` (`user_password`),
+  ADD UNIQUE KEY `user_email` (`user_email`),
+  ADD UNIQUE KEY `user_role` (`user_role`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_attraction`
+--
+ALTER TABLE `tbl_attraction`
+  MODIFY `atrr_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_city`
+--
+ALTER TABLE `tbl_city`
+  MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_comment`
+--
+ALTER TABLE `tbl_comment`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_region`
+--
+ALTER TABLE `tbl_region`
+  MODIFY `region_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
