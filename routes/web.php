17c93fa70cbd748b16bf4d8678e757761c1c464a
AnuKun <118412797+AnpTeam@users.noreply.github.com>
2025-09-04 14:46:50 +0700

    [config] Update web.php (Attractions)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AttractionController;
 
 /* HOME PAGE */
 Route::get('/', [UserController::class, 'index']);
@@ -20,4 +21,22 @@ Route::get('/user/reset/{id}',  [UserController::class, 'reset']);
 Route::put('/user/reset/{id}',  [UserController::class, 'resetPassword']);
 /* USER ROUTE END */
 
+/** ATTRACTION ROUTE
+ *  @USAGE : BASIC CRUD (CREATE, READ, UPDATE, DELETE)
+ */
+Route::get('/attraction', [AttractionController::class, 'index']);
+Route::get('/attraction/adding',  [AttractionController::class, 'adding']);
+Route::post('/attraction',  [AttractionController::class, 'create']);
+Route::get('/attraction/{id}',  [AttractionController::class, 'edit']);
+Route::put('/attraction/{id}', [AttractionController::class, 'update']);
+Route::delete('/attraction/remove/{id}',  [AttractionController::class, 'remove']);
+/* ATTRACTION ROUTE END */
+
+//product crud
+Route::get('/product', [ProductController::class, 'index']);
+Route::get('/product/adding',  [ProductController::class, 'adding']);
+Route::post('/product',  [ProductController::class, 'create']);
+Route::get('/product/{id}',  [ProductController::class, 'edit']);
+Route::put('/product/{id}',  [ProductController::class, 'update']);
+Route::delete('/product/remove/{id}',  [ProductController::class, 'remove']);
 
